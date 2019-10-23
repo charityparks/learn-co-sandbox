106beaf70da02a37fd3561384972bbd2033b0dc0
Charity Parks <tuppercharity@gmail.com>
2019-10-23 20:32:52 +0000

    testing

--- cheesecake_project/lib/cheesecake_project.rb
+++ cheesecake_project/lib/cheesecake_project.rb
@@ -1,9 +1,4 @@
-#this is the environment file
-
 require "cheesecake_project/version"
-require_relative "./cheesecake_project/version"
-require_relative "./cheesecake_project/cli"
-require "pry"
 
 module CheesecakeProject
   class Error < StandardError; end
