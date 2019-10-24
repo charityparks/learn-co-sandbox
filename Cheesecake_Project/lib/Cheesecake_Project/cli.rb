43b9e07334649b6dacb30458c638c2198fd10c78
Charity Parks <tuppercharity@gmail.com>
2019-10-24 19:22:53 +0000

    yay

--- Cheesecake_Project/lib/Cheesecake_Project/cli.rb
+++ Cheesecake_Project/lib/Cheesecake_Project/cli.rb
@@ -1,6 +1,19 @@
 class CheesecakeProject::CLI
   def call
     puts "Enjoy these recipes!"
+    get_recipes
+    list_recipes
   end
+  
+  def get_recipes
+    @recipes = ["Caramel Macchiato Cheesecake", "Apple Cheesecake with Caramel Sauce", "Turtles® Cheesecake", "Double Layer Pumpkin Cheesecake"]
+  end
+  # The @recipes is a class variable 
+  
+  def list_recipes
+    @recipes.each.with_index(1) do |recipe, index| 
+            puts "#{index}.#{recipe}"
+          end
+        end
   
 end
