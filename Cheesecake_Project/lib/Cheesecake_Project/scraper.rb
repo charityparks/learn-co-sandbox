fd1d96d51161415678f78ce617d1cfcf39e0ffcf
Charity Parks <tuppercharity@gmail.com>
2019-10-28 03:57:38 +0000

    scraper save

--- Cheesecake_Project/lib/Cheesecake_Project/scraper.rb
+++ Cheesecake_Project/lib/Cheesecake_Project/scraper.rb
@@ -1,8 +1,9 @@
 class CheesecakeProject::Scraper
   
   def self.scrape_recipes   #class method
-    doc = Nokogiri::HTML(open("http://dish.allrecipes.com/favorite-fall-cheesecakes/?internalSource=streams&referringId=387&referringContentType=Recipe%20Hub&clickId=st_trending_b")
-    recipes = doc.css(<h1 id="recipe-main-content")
+    doc = Nokogiri::HTML(open("http://dish.allrecipes.com/favorite-fall-cheesecakes"))
+    
+    recipes = doc.css("h1#recipe-main-content")
     
     recipes.each do |r|
       name = r.text
