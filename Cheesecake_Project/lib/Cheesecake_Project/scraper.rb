5e8b32403b674f4a678f272bc00f23e9a6279dd1
Charity Parks <tuppercharity@gmail.com>
2019-10-29 01:30:59 +0000

    update scraper

--- Cheesecake_Project/lib/Cheesecake_Project/scraper.rb
+++ Cheesecake_Project/lib/Cheesecake_Project/scraper.rb
@@ -3,13 +3,27 @@ class CheesecakeProject::Scraper
   def self.scrape_recipes   #class method
     doc = Nokogiri::HTML(open("http://dish.allrecipes.com/favorite-fall-cheesecakes"))
     
-    recipes = doc.css("h1#recipe-main-content")
+    recipes = doc.css(".entry-content h3")
     
     recipes.each do |r|
-      name = r.text
+      name = r.css("a").text
+      name = r.css("a").attribute("href").value 
       
-      CheesecakeProject::Recipes.new(name)
+      CheesecakeProject::Recipes.new(name, url)
     end
+  end
+  def self.scrape_ingredients(recipe) #receiving an object/instance 
+    doc = Nokogiri::HTML(open(recipe.url))
+    
+    ingredients = doc.css("#lst_ingredients_1 li")
+    ingredients2 = doc.css("#lst_ingredients_2 li")
+    
+    ingredients.each do |ingredient|
+      name = ingredient.css("span").text 
+      recipe.ingredient << name 
+    end
+    
+    recipe.ingredients.pop
   end
   
 end
