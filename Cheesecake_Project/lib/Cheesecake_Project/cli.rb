9dfc3ce8c2f1972e99f7012022b8e76326f4c223
Charity Parks <tuppercharity@gmail.com>
2019-11-15 22:11:09 +0000

    Automatically backed up by Learn

--- Cheesecake_Project/lib/Cheesecake_Project/cli.rb
+++ Cheesecake_Project/lib/Cheesecake_Project/cli.rb
@@ -71,7 +71,7 @@ class CheesecakeProject::Cli      #This cli belongs to the CheesecakeProject. He
     end 
     
     def view_urls
-       @recipes.collect do |recipe, url|
+       @recipes.collect do |url|
        puts "#{recipe.url}"
      end
       
